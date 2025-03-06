34bde67ad15bc4cb183082f27812c6aca771eaaf
BarnabyRoss <chentong0547@outlook.com>
2025-03-06 20:36:54 +0800

    模版函数的实现放在头文件中

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -27,26 +27,6 @@ void ThreadPool::work(){
 
 }
 
-template< typename F, typename... Args >
-auto ThreadPool::submit(F&& func, Args&&... args) -> std::future<std::invoke_result_t<F, Args...>>{
-	
-	using ReturnType = std::invoke_result_t<F, Args...>;
-	auto prom = std::make_shared<std::promise<ReturnType>>();
-	auto fut = prom->get_future();
-
-	auto task = [prom, func = std::forward<F>(func), ...args = std::forward<Args>(args)](){
-		try{
-			prom->set_value(func(args...));
-		}catch(...){
-			prom->set_execption(std::current_exception());
-		}
-	};
-
-	taskQueue_.push(std::move(task));
-
-	return fut;
-}
-
 /*void ThreadPool::stop(){
 
 	stop_ = true;
--- ThreadPool.h
+++ ThreadPool.h
@@ -22,7 +22,24 @@ public:
   ~ThreadPool();
 	
 	template< typename F, typename... Args >
-	auto submit(F&& func, Args... args) -> std::future<std::invoke_result_t<F, Args...>>;
+	auto submit(F&& func, Args&&... args) -> std::future<std::invoke_result_t<F, Args...>>{
+
+		using ReturnType = std::invoke_result_t<F, Args...>;
+		auto prom = std::make_shared<std::promise<ReturnType>>();
+		auto fut = prom->get_future();
+
+		auto task = [prom, func = std::forward<F>(func), ...args = std::forward<Args>(args)](){
+			try{
+				prom->set_value(func(args...));
+			}catch(...){
+				prom->set_execption(std::current_exception());
+			}
+		};
+
+		taskQueue_.push(std::move(task));
+
+		return fut;
+	}
 	//void stop();
 	void shutdown();
 };
